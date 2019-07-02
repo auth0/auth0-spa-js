f0d10af06cb3beeafe761014fb19932da2fe5465
Luis Deschamps Rudge <luis@luisrudge.net>
2019-07-02 12:06:24 -0300

    Prepare 1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## [v1.0.2](https://github.com/auth0/auth0-spa-js/tree/v1.0.2) (2019-07-02)
+
+**Changed**
+
+- Add polyfill for TextEncoder - https://github.com/auth0/auth0-spa-js/pull/46
+
 ## [v1.0.1](https://github.com/auth0/auth0-spa-js/tree/v1.0.1) (2019-06-24)
 
 **Changed**
--- scripts/release.js
+++ scripts/release.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
 const pkg = require('../package.json');
+const exec = require('./exec');
+
 const newVersion = process.argv[2];
 if (!newVersion) {
   throw new Error('usage: `release new_version`');
 }
 
-const newReadme = fs
-  .readFileSync('./README.md')
-  .toString()
-  .replace(pkg.version, newVersion);
+(async () => {
+  await exec('git checkout master');
+  await exec('git pull');
+  await exec(`git checkout -b prepare/${newVersion}`);
 
-fs.writeFileSync('./README.md', newReadme);
+  const newReadme = fs
+    .readFileSync('./README.md')
+    .toString()
+    .replace(pkg.version, newVersion);
 
-fs.writeFileSync(
-  './package.json',
-  JSON.stringify({ ...pkg, version: newVersion }, null, 2)
-);
-fs.writeFileSync('./src/version.ts', `export default '${newVersion}';`);
+  fs.writeFileSync('./README.md', newReadme);
+
+  fs.writeFileSync(
+    './package.json',
+    JSON.stringify({ ...pkg, version: newVersion }, null, 2)
+  );
+  fs.writeFileSync('./src/version.ts', `export default '${newVersion}';`);
+
+  await exec('npm run docs');
+})();
