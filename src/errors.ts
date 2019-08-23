d744e480421a956994e0f4001abcf7ff0f5cfb0c
Luis Deschamps Rudge <luis@luisrudge.net>
2019-08-23 20:37:17 -0300

    fix ts

--- src/errors.ts
+++ src/errors.ts
@@ -14,5 +14,7 @@ export class InternalError extends Error {
   public error = 'internal_error';
   constructor(public error_description: string = 'Internal Error') {
     super(error_description);
+    //https://github.com/Microsoft/TypeScript-wiki/blob/master/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, InternalError.prototype);
   }
 }
