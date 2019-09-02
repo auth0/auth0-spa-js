ede0a7ca5af33c6a08a1ac673da0f2191bb8e719
Luis Deschamps Rudge <luis@luisrudge.net>
2019-09-02 17:23:29 -0300

    don't encode undefined values

--- src/utils.ts
+++ src/utils.ts
@@ -103,6 +103,7 @@ export const decodeState = (state: string) => atob(state);
 
 export const createQueryParams = (params: any) => {
   return Object.keys(params)
+    .filter(k => typeof params[k] !== 'undefined')
     .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
     .join('&');
 };
