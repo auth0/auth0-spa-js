c1d5f1920ccc3ff896bb0ef850d8922936904773
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-01-06 15:40:02 +0000

    [SDK-1178] Local Storage caching mechanism (#303)
    * Refactored existing cache
    
    * Renamed to InMemoryCache
    * Removed default export
    * Added ICache
    * Refactored tests
    * Auth0Client now uses ICache instead of implemented cache type
    
    * Added ability to configure cache strategy
    
    * Added VS Code debug configuration for running tests
    
    * Implemented local storage cache
    
    * Made use of "in" to check presence of key in object
    
    * Refactored playground page to use Vue + Bootstrap
    
    * Added ability to clear the token cache
    
    Added clear() to the ICache interface, meaning that this had to be
    applied to the memory and localstorage caches.
    
    Made use of jest-localstorage-mock package for easier testing with
    localstorage, making the clear method easier to test for the
    LocalStorageCache implementation.
    
    * Revamped the playground page with Vue and Bootstrap additions
    
    * Vulnerable dependency update
    
    * Added a section in the readme about the caching strategy
    
    * Fixed integration tests
    
    * Readme wording
    
    * Refactored how items are cleared from local storage
    
    * Refactored cache key
    
    * Readme tweak to make what 'data' is more clear
    
    * Renamed cacheStrategy option to cacheLocation
    
    * Cache now includes client_id in key

--- src/cache.ts
+++ src/cache.ts
@@ -1,6 +1,7 @@
 interface CacheKeyData {
   audience: string;
   scope: string;
+  client_id: string;
 }
 
 interface DecodedToken {
@@ -8,18 +9,29 @@ interface DecodedToken {
   user: any;
 }
 
-interface CacheEntry extends CacheKeyData {
+interface CacheEntry {
   id_token: string;
   access_token: string;
   expires_in: number;
   decodedToken: DecodedToken;
+  audience: string;
+  scope: string;
+  client_id: string;
 }
 
 interface CachedTokens {
   [key: string]: CacheEntry;
 }
 
-const createKey = (e: CacheKeyData) => `${e.audience}::${e.scope}`;
+export interface ICache {
+  save(entry: CacheEntry): void;
+  get(key: CacheKeyData): CacheEntry;
+  clear(): void;
+}
+
+const keyPrefix = '@@auth0spajs@@';
+const createKey = (e: CacheKeyData) =>
+  `${keyPrefix}::${e.client_id}::${e.audience}::${e.scope}`;
 
 const getExpirationTimeoutInMilliseconds = (expiresIn: number, exp: number) => {
   const expTime =
@@ -27,20 +39,83 @@ const getExpirationTimeoutInMilliseconds = (expiresIn: number, exp: number) => {
   return Math.min(expiresIn, expTime) * 1000 * 0.8;
 };
 
-export default class Cache {
-  cache: CachedTokens = {};
-  public save(entry: CacheEntry) {
-    const key = createKey(entry);
-    this.cache[key] = entry;
+export class LocalStorageCache implements ICache {
+  public save(entry: CacheEntry): void {
+    const cacheKey = createKey(entry);
+    const expiresInTime = Math.floor(Date.now() / 1000) + entry.expires_in;
+
+    const expirySeconds =
+      Math.min(expiresInTime, entry.decodedToken.claims.exp) - 60; // take off a small leeway
+
+    const payload = {
+      body: entry,
+      expiresAt: expirySeconds
+    };
+
     const timeout = getExpirationTimeoutInMilliseconds(
       entry.expires_in,
       entry.decodedToken.claims.exp
     );
+
+    setTimeout(() => {
+      window.localStorage.removeItem(cacheKey);
+    }, timeout);
+
+    window.localStorage.setItem(cacheKey, JSON.stringify(payload));
+  }
+
+  public get(key: CacheKeyData): CacheEntry {
+    const cacheKey = createKey(key);
+    const json = window.localStorage.getItem(cacheKey);
+    let payload;
+
+    if (!json) return;
+
+    payload = JSON.parse(json);
+
+    if (!payload) return;
+
+    const nowSeconds = Math.floor(Date.now() / 1000);
+
+    if (payload.expiresAt < nowSeconds) {
+      window.localStorage.removeItem(cacheKey);
+      return;
+    }
+
+    return payload.body;
+  }
+
+  public clear() {
+    for (var i = localStorage.length - 1; i >= 0; i--) {
+      if (localStorage.key(i).startsWith(keyPrefix)) {
+        localStorage.removeItem(localStorage.key(i));
+      }
+    }
+  }
+}
+
+export class InMemoryCache implements ICache {
+  cache: CachedTokens = {};
+
+  public save(entry: CacheEntry) {
+    const key = createKey(entry);
+    this.cache[key] = entry;
+
+    const timeout = getExpirationTimeoutInMilliseconds(
+      entry.expires_in,
+      entry.decodedToken.claims.exp
+    );
+
     setTimeout(() => {
       delete this.cache[key];
     }, timeout);
   }
+
   public get(key: CacheKeyData) {
     return this.cache[createKey(key)];
+  }
+
+  public clear() {
+    this.cache = {};
   }
 }
