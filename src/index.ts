23a9452f592e6f6e85cee373ade84149784f7e7b
adamjmcgrath <adam.mcgrath@auth0.com>
2020-03-19 16:28:15 +0000

    Revert "Revert "[SDK-1379] Export constructor" (#382)"
    This reverts commit 178b65f30c9b7a76ce762a1b4de1bd3ccfca5e5a.
    
    # Conflicts:
    #	__tests__/index.test.ts
    #	src/Auth0Client.ts
    #	src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,22 @@ import 'promise-polyfill/src/polyfill';
 import 'fast-text-encoding';
 import 'abortcontroller-polyfill/dist/abortcontroller-polyfill-only';
 
-import Auth0Client_ from './Auth0Client';
+import Auth0Client from './Auth0Client';
 import * as ClientStorage from './storage';
 import { Auth0ClientOptions } from './global';
 import { CACHE_LOCATION_MEMORY } from './constants';
 
 import './global';
-import { validateCrypto } from './utils';
 import { getUniqueScopes } from './utils';
 
 export * from './global';
 
 export default async function createAuth0Client(options: Auth0ClientOptions) {
-  validateCrypto();
-
   if (options.useRefreshTokens) {
     options.scope = getUniqueScopes(options.scope, 'offline_access');
   }
 
-  const auth0 = new Auth0Client_(options);
+  const auth0 = new Auth0Client(options);
 
   if (
     auth0.cacheLocation === CACHE_LOCATION_MEMORY &&
@@ -45,4 +42,4 @@ export default async function createAuth0Client(options: Auth0ClientOptions) {
   return auth0;
 }
 
-export type Auth0Client = Auth0Client_;
+export { Auth0Client };
