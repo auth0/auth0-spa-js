1054ba48d8054a97dda0ed6e6fbc83fd46b6b8f1
Ryan Wolfe <rn.wolfe@gmail.com>
2020-10-12 11:15:38 -0400

    refer to latest mock result

--- __tests__/helpers.ts
+++ __tests__/helpers.ts
@@ -1,5 +1,5 @@
 export const expectToHaveBeenCalledWithAuth0ClientParam = (mock, expected) => {
-  const [[url]] = (<jest.Mock>mock).mock.calls;
+  const url = (<jest.Mock>mock).mock.calls[mock.mock.calls.length - 1][0];
   const param = new URL(url).searchParams.get('auth0Client');
   const decodedParam = decodeURIComponent(atob(param));
   const actual = JSON.parse(decodedParam);
