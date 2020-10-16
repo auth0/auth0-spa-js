5f53bdcbd3d7fd968cef8c82bf0c8b84cdf113cf
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-10-16 16:57:59 +0100

    Add test for login flow with session storage + cookies

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,7 +1,12 @@
 module.exports.shouldBeUndefined = e => expect(e).to.be.undefined;
 module.exports.shouldNotBeUndefined = e => expect(e).to.not.be.undefined;
 module.exports.shouldBe = (expected, actual) => expect(actual).to.eq(expected);
+
+module.exports.shouldInclude = (expected, actual) =>
+  expect(actual).to.include(actual);
+
 module.exports.shouldNotBe = (expected, actual) =>
   expect(actual).to.not.eq(expected);
+
 module.exports.whenReady = () =>
   cy.get('#loaded', { timeout: 10000 }).then(() => cy.window());
