96ccaa48fc499bbc164856872180545f0fe1f0b3
frederikprijck <frederik.prijck@auth0.com>
2020-11-03 09:37:09 +0100

    [SDK-2049] Move and refactor getIdTokenClaims tests

--- __tests__/Auth0Client.test.ts
+++ __tests__/Auth0Client.test.ts
@@ -617,18 +617,9 @@ describe('Auth0Client', () => {
       );
       expect(await auth0.getUser({ scope: 'foo' })).toEqual(expectedUser);
       expect(await auth0.getUser({ audience: 'invalid' })).toBeUndefined();
-      expect(await auth0.getIdTokenClaims()).toBeTruthy();
-      expect(await auth0.getIdTokenClaims({})).toBeTruthy();
-      expect(
-        await auth0.getIdTokenClaims({ audience: 'default' })
-      ).toBeTruthy();
-      expect(await auth0.getIdTokenClaims({ scope: 'foo' })).toBeTruthy();
-      expect(
-        await auth0.getIdTokenClaims({ audience: 'invalid' })
-      ).toBeUndefined();
     });
 
-    it('should log the user in with custom scope', async () => {
+    it('should log the user in and get the user with custom scope', async () => {
       const auth0 = setup({
         scope: 'scope1',
         advancedOptions: {
@@ -1436,6 +1427,150 @@ describe('Auth0Client', () => {
 
       const decodedToken = await auth0.getUser();
       expect(decodedToken).toBeUndefined();
+    });
+  });
+
+  describe('getIdTokenClaims', () => {
+    describe('when loginWithRedirect', () => {
+      it('returns undefined if there is no cache', async () => {
+        const auth0 = setup();
+
+        jest.spyOn(auth0['cache'], 'get').mockReturnValueOnce(undefined);
+
+        const decodedToken = await auth0.getIdTokenClaims();
+        expect(decodedToken).toBeUndefined();
+      });
+
+      it('returns the ID token claims', async () => {
+        const auth0 = setup({ scope: 'foo' });
+        await loginWithRedirect(auth0);
+
+        expect(await auth0.getIdTokenClaims()).toHaveProperty('exp');
+        expect(await auth0.getIdTokenClaims()).not.toHaveProperty('me');
+        expect(await auth0.getIdTokenClaims({})).toHaveProperty('exp');
+        expect(
+          await auth0.getIdTokenClaims({ audience: 'default' })
+        ).toHaveProperty('exp');
+        expect(await auth0.getIdTokenClaims({ scope: 'foo' })).toHaveProperty(
+          'exp'
+        );
+        expect(
+          await auth0.getIdTokenClaims({ audience: 'invalid' })
+        ).toBeUndefined();
+      });
+
+      it('returns the ID token claims with custom scope', async () => {
+        const auth0 = setup({
+          scope: 'scope1',
+          advancedOptions: {
+            defaultScope: 'scope2'
+          }
+        });
+        await loginWithRedirect(auth0, { scope: 'scope3' });
+
+        expect(
+          await auth0.getIdTokenClaims({ scope: 'scope1 scope2 scope3' })
+        ).toHaveProperty('exp');
+      });
+
+      describe('when using refresh tokens', () => {
+        it('returns the ID token claims with offline_access', async () => {
+          const auth0 = setup({ scope: 'foo', useRefreshTokens: true });
+          await loginWithRedirect(auth0);
+
+          expect(
+            await auth0.getIdTokenClaims({ scope: 'foo offline_access' })
+          ).toHaveProperty('exp');
+        });
+
+        it('returns the ID token claims with custom scope and offline_access', async () => {
+          const auth0 = setup({
+            scope: 'scope1',
+            advancedOptions: {
+              defaultScope: 'scope2'
+            },
+            useRefreshTokens: true
+          });
+          await loginWithRedirect(auth0, { scope: 'scope3' });
+
+          expect(
+            await auth0.getIdTokenClaims({
+              scope: 'scope1 scope2 scope3 offline_access'
+            })
+          ).toHaveProperty('exp');
+        });
+      });
+    });
+
+    describe('when loginWithPopup', () => {
+      it('returns undefined if there is no cache', async () => {
+        const auth0 = setup();
+
+        jest.spyOn(auth0['cache'], 'get').mockReturnValueOnce(undefined);
+
+        const decodedToken = await auth0.getIdTokenClaims();
+        expect(decodedToken).toBeUndefined();
+      });
+
+      it('returns the ID token claims', async () => {
+        const auth0 = setup({ scope: 'foo' });
+        await loginWithPopup(auth0);
+
+        expect(await auth0.getIdTokenClaims()).toHaveProperty('exp');
+        expect(await auth0.getIdTokenClaims()).not.toHaveProperty('me');
+        expect(await auth0.getIdTokenClaims({})).toHaveProperty('exp');
+        expect(
+          await auth0.getIdTokenClaims({ audience: 'default' })
+        ).toHaveProperty('exp');
+        expect(await auth0.getIdTokenClaims({ scope: 'foo' })).toHaveProperty(
+          'exp'
+        );
+        expect(
+          await auth0.getIdTokenClaims({ audience: 'invalid' })
+        ).toBeUndefined();
+      });
+
+      it('returns the ID token claims with custom scope', async () => {
+        const auth0 = setup({
+          scope: 'scope1',
+          advancedOptions: {
+            defaultScope: 'scope2'
+          }
+        });
+        await loginWithPopup(auth0, { scope: 'scope3' });
+
+        expect(
+          await auth0.getIdTokenClaims({ scope: 'scope1 scope2 scope3' })
+        ).toHaveProperty('exp');
+      });
+
+      describe('when using refresh tokens', () => {
+        it('returns the ID token claims with offline_access', async () => {
+          const auth0 = setup({ scope: 'foo', useRefreshTokens: true });
+          await loginWithPopup(auth0);
+
+          expect(
+            await auth0.getIdTokenClaims({ scope: 'foo offline_access' })
+          ).toHaveProperty('exp');
+        });
+
+        it('returns the ID token claims with custom scope and offline_access', async () => {
+          const auth0 = setup({
+            scope: 'scope1',
+            advancedOptions: {
+              defaultScope: 'scope2'
+            },
+            useRefreshTokens: true
+          });
+          await loginWithPopup(auth0, { scope: 'scope3' });
+
+          expect(
+            await auth0.getIdTokenClaims({
+              scope: 'scope1 scope2 scope3 offline_access'
+            })
+          ).toHaveProperty('exp');
+        });
+      });
     });
   });
 });
