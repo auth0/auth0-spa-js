509dc3c7fbbf1b6f1cd271d74ad330c674a454e7
Steve Hobbs <steve.hobbs.mail@gmail.com>
2020-11-12 12:13:43 +0000

    Heed timeoutInSeconds when calling getTokenSilently with refresh tokens

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1120,7 +1120,10 @@ describe('Auth0', () => {
 
           cache.get.mockReturnValue({ refresh_token: TEST_REFRESH_TOKEN });
 
-          await auth0.getTokenSilently({ ignoreCache: true });
+          await auth0.getTokenSilently({
+            ignoreCache: true,
+            timeoutInSeconds: 10
+          });
 
           expect(cache.get).toHaveBeenCalledWith({
             audience: 'default',
@@ -1136,7 +1139,8 @@ describe('Auth0', () => {
               refresh_token: TEST_REFRESH_TOKEN,
               client_id: TEST_CLIENT_ID,
               grant_type: 'refresh_token',
-              redirect_uri: 'http://localhost'
+              redirect_uri: 'http://localhost',
+              timeout: 10000
             },
             webWorkerMatcher
           );
