facb23548a46c3c5c36f74c22582c46a6cdb41eb
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-01-05 11:32:32 +0000

    Add coverage for testing browsers that support workers

--- __tests__/Auth0Client/getTokenSilently.test.ts
+++ __tests__/Auth0Client/getTokenSilently.test.ts
@@ -617,12 +617,19 @@ describe('Auth0Client', () => {
           userAgent:
             'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
           supported: false
+        },
+        {
+          name: 'Chrome',
+          userAgent:
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+          supported: true
         }
       ].forEach(({ name, userAgent, supported }) =>
         it(`refreshes the token ${
           supported ? 'with' : 'without'
         } the worker, when ${name}`, async () => {
           const originalUserAgent = window.navigator.userAgent;
+
           Object.defineProperty(window.navigator, 'userAgent', {
             value: userAgent,
             configurable: true
