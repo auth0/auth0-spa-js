594677a0a3099b7db21f3ffd0328ece6f6056d06
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-06-02 17:35:52 +0100

    Add key removal + tests

--- __tests__/cache/key-manifest.test.ts
+++ __tests__/cache/key-manifest.test.ts
@@ -28,7 +28,7 @@ describe('CacheKeyManifest', () => {
     expect(entry.keys).toStrictEqual([key.toKey()]);
   });
 
-  it('should add another entry to the same list if one already exists in the manifest', async () => {
+  it('should add another key to the same list if an entry already exists in the manifest', async () => {
     const key = new CacheKey({
       client_id: TEST_CLIENT_ID,
       audience: TEST_AUDIENCE,
@@ -49,5 +49,134 @@ describe('CacheKeyManifest', () => {
 
     expect(entry.keys).toHaveLength(2);
     expect(entry.keys).toStrictEqual([key.toKey(), key2.toKey()]);
+  });
+
+  it('should not add the same key twice', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+
+    const key2 = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: 'http://another-audience',
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key2);
+    await manifest.add(key2);
+
+    const entry = await manifest.get(key);
+
+    // Should still only have 2 keys, despite adding key, key2 and key2 again
+    expect(entry.keys).toHaveLength(2);
+    expect(entry.keys).toStrictEqual([key.toKey(), key2.toKey()]);
+  });
+
+  it('should add another entry for a different client ID', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+
+    const key2 = new CacheKey({
+      client_id: 'some-other-client',
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key2);
+
+    expect((await manifest.get(key)).keys).toStrictEqual([key.toKey()]);
+    expect((await manifest.get(key2)).keys).toStrictEqual([key2.toKey()]);
+  });
+
+  it('can remove an entry', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+    await manifest.remove(key);
+    expect(await manifest.get(key)).toBeFalsy();
+  });
+
+  it('does nothing if trying to remove an item that does not exist', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await expect(manifest.remove(key)).resolves.toBeFalsy();
+  });
+
+  it('can remove a key from an entry and leave others intact', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    const key2 = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: 'http://another-audience',
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+    await manifest.add(key2);
+
+    await manifest.remove(key);
+    expect((await manifest.get(key)).keys).toStrictEqual([key2.toKey()]);
+  });
+
+  it('can remove an entry and leave other entries intact', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    const key2 = new CacheKey({
+      client_id: 'some-other-client',
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+    await manifest.add(key2);
+    await manifest.remove(key);
+
+    expect(await manifest.get(key)).toBeFalsy();
+    expect((await manifest.get(key2)).keys).toStrictEqual([key2.toKey()]);
+  });
+
+  it('does not remove the whole entry if the key was not found', async () => {
+    const key = new CacheKey({
+      client_id: TEST_CLIENT_ID,
+      audience: TEST_AUDIENCE,
+      scope: TEST_SCOPES
+    });
+
+    await manifest.add(key);
+
+    await manifest.remove(
+      new CacheKey({
+        client_id: key.client_id,
+        audience: 'http://some-other-audience',
+        scope: key.scope
+      })
+    );
+
+    expect((await manifest.get(key)).keys).toStrictEqual([key.toKey()]);
   });
 });
--- src/cache/key-manifest.ts
+++ src/cache/key-manifest.ts
@@ -23,7 +23,25 @@ export class CacheKeyManifest {
     }
   }
 
-  remove(key: CacheKey) {}
+  async remove(key: CacheKey): Promise<void> {
+    const manifestKey = this.createManifestKeyFrom(key);
+    const existingEntry = await this.cache.get<KeyManifestEntry>(manifestKey);
+
+    if (existingEntry) {
+      const itemKey = key.toKey();
+      const index = existingEntry.keys.indexOf(itemKey);
+
+      if (index > -1) {
+        existingEntry.keys.splice(index, 1);
+      }
+
+      if (existingEntry.keys.length > 0) {
+        return await this.cache.set(manifestKey, existingEntry);
+      }
+
+      return await this.cache.remove(manifestKey);
+    }
+  }
 
   get(key: CacheKey): Promise<KeyManifestEntry> {
     const manifestKey = this.createManifestKeyFrom(key);
