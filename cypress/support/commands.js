dcab4e9b5642ec88a61ad573ed6350a9ed476920
Frederik <frederik.prijck@gmail.com>
2021-08-03 11:07:07 +0200

    Use dummy password

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -22,7 +22,7 @@ const login = () => {
 
   cy.get('.login-card input[name=password]')
     .clear()
-    .type(Cypress.env('INTEGRATION_PASSWORD'));
+    .type(Cypress.env('INTEGRATION_PASSWORD') || '1234');
 
   cy.get('.login-submit').click();
   // Need to click one more time to give consent.
