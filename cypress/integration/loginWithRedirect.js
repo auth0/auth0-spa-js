59c907b83bd9d9dec98e9dec1ca88e2555ff1470
Steve Hobbs <steve.hobbs.mail@gmail.com>
2021-09-15 13:49:51 +0100

    Fix E2E tests for login

--- cypress/integration/loginWithRedirect.js
+++ cypress/integration/loginWithRedirect.js
@@ -1,4 +1,4 @@
-import { whenReady, shouldInclude, tolerance } from '../support/utils';
+import { whenReady, shouldInclude, tolerance, get } from '../support/utils';
 
 describe('loginWithRedirect', function () {
   beforeEach(cy.resetTests);
@@ -11,10 +11,16 @@ describe('loginWithRedirect', function () {
       cy.url().should(url => shouldInclude(url, 'http://127.0.0.1:3000'));
 
       whenReady().then(win => {
-        expect(win.sessionStorage.getItem('a0.spajs.txs')).to.exist;
+        get('client-id').then($clientIdBox => {
+          expect(
+            win.sessionStorage.getItem(`a0.spajs.txs.${$clientIdBox.val()}`)
+          ).to.exist;
 
-        cy.handleRedirectCallback().then(() => {
-          expect(win.sessionStorage.getItem('a0.spajs.txs')).to.not.exist;
+          cy.handleRedirectCallback().then(() => {
+            expect(
+              win.sessionStorage.getItem(`a0.spajs.txs.${$clientIdBox.val()}`)
+            ).to.not.exist;
+          });
         });
       });
     });
@@ -30,16 +36,18 @@ describe('loginWithRedirect', function () {
     cy.url().should(url => shouldInclude(url, 'http://127.0.0.1:3000'));
     whenReady();
 
-    cy.getCookie('a0.spajs.txs')
-      .should('exist')
-      .should(cookie => {
-        // Check that the cookie value is at least within a second of what we expect, to make
-        // the test a little less brittle.
-        expect(tolerance(cookie.expiry, tomorrowInSeconds, 1)).to.be.true;
-      });
+    get('client-id').then($clientIdBox => {
+      cy.getCookie(`a0.spajs.txs.${$clientIdBox.val()}`)
+        .should('exist')
+        .should(cookie => {
+          // Check that the cookie value is at least within a second of what we expect, to make
+          // the test a little less brittle.
+          expect(tolerance(cookie.expiry, tomorrowInSeconds, 1)).to.be.true;
+        });
 
-    cy.handleRedirectCallback().then(() => {
-      cy.getCookie('a0.spajs.txs').should('not.exist');
+      cy.handleRedirectCallback().then(() => {
+        cy.getCookie(`a0.spajs.txs.${$clientIdBox.val()}`).should('not.exist');
+      });
     });
   });
 });
