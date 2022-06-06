439dbd8512fa9cdeb9a6792cba5dc64f2d7d4ae2
William Shepherd <will@codeincolor.io>
2022-06-05 20:35:25 -0500

    Accept the other possible value

--- src/global.ts
+++ src/global.ts
@@ -45,7 +45,7 @@ export interface BaseLoginOptions {
    *
    * This only affects the New Universal Login Experience.
    */
-  screen_hint?: 'signup';
+  screen_hint?: 'signup' | 'login';
 
   /**
    * The user's email address or other identifier. When your app knows
