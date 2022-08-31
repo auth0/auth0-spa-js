ec8709d93dedc8901e7f0ab5a179d437ccfb7914
frederikprijck <frederik.prijck@auth0.com>
2022-08-31 23:31:25 +0200

    Update tests and ignore in cypress

--- cypress/integration/migration.js
+++ cypress/integration/migration.js
@@ -1,166 +1,147 @@
+Cypress.Commands.add('useV1', ({ clientScope, foo }) => {
+  cy.visit('http://localhost:3000/v1.html');
+
+  // create client
+  clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
+  foo && cy.get('[data-cy=foo]').clear().type(foo);
+  cy.get('[data-cy=create-client]').click();
+
+  // logout
+  cy.get('[data-cy=logout]').click();
+});
+
+Cypress.Commands.add('loginV1', ({ loginScope, clientScope }) => {
+  // login
+  // set login scope
+  loginScope && cy.get('[data-cy=login-scope]').clear().type(loginScope);
+  cy.get('[data-cy=login]').click();
+
+  // create client
+  clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
+  cy.get('[data-cy=create-client]').click();
+
+  // handle callback
+  cy.get('[data-cy=handle-redirect-callback]').click();
+
+  // expect user to show up
+  cy.get('[data-cy=profile]').should('exist');
+});
+
+Cypress.Commands.add('useV2', ({ clientScope, foo }) => {
+  cy.visit('http://localhost:3000/v2.html');
+
+  // create client
+  clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
+  foo && cy.get('[data-cy=foo]').clear().type(foo);
+  cy.get('[data-cy=create-client]').click();
+});
+
 describe('migration', function () {
-  //beforeEach(cy.resetTests);
-  //afterEach(cy.fixCookies);
-
-  it('runs scenario 1 (without foo)', function () {
+  it('Should migrate without setting any scopes', function () {
     cy.server();
 
-    runScenarioWithoutFoo({});
-  });
+    cy.useV1({});
+    cy.loginV1({});
 
-  it('runs scenario 2 (without foo)', function () {
-    cy.server();
-    runScenarioWithoutFoo({ clientScope: 'Scope1' });
-  });
+    cy.useV2({});
 
-  it('runs scenario 3 (without foo)', function () {
-    cy.server();
-
-    runScenarioWithoutFoo({ loginScope: 'Scope2' });
-  });
-
-  it('runs scenario 4 (without foo)', function () {
-    cy.server();
-
-    runScenarioWithoutFoo({ clientScope: 'Scope1', loginScope: 'Scope2' });
-  });
-
-  it('runs scenario 1 (with foo)', function () {
-    cy.server();
-
-    runScenarioWithFoo({ foo: 'bar123' });
-  });
-
-  it('runs scenario 2 (with foo)', function () {
-    cy.server();
-    runScenarioWithFoo({ clientScope: 'Scope1', foo: 'bar123' });
-  });
-
-  it('runs scenario 3 (with foo)', function () {
-    cy.server();
-
-    runScenarioWithFoo({ loginScope: 'Scope2', foo: 'bar123' });
-  });
-
-  it('runs scenario 4 (with foo)', function () {
-    cy.server();
-
-    runScenarioWithFoo({
-      clientScope: 'Scope1',
-      loginScope: 'Scope2',
-      foo: 'bar123'
-    });
-  });
-
-  it('runs scenario 5 (without foo)', function () {
-    cy.server();
-
-    runScenarioWithoutFoo({
-      clientScope: 'Scope1 Scope2',
-      loginScope: 'Scope3'
-    });
-
-    // go to v2
-    cy.visit('http://localhost:3000/v2.html');
-
-    // create client
-    cy.get('[data-cy=client-scope]').clear().type('Scope1 Scope2');
-    cy.get('[data-cy=create-client]').click();
-
-    cy.get('[data-cy=get-token-scope]').clear().type('Scope1 Scope2');
-    cy.get('[data-cy=get-token]').click();
+    cy.get('[data-cy=profile]').should('exist');
     cy.get('[data-cy=profile]').should('not.contain', 'bar123');
 
-    // go to v2
-    cy.visit('http://localhost:3000/v2.html');
+    cy.get('[data-cy=get-token]').click();
+    //cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
 
-    // create client
-    cy.get('[data-cy=client-scope]').clear().type('Scope1 Scope2');
-    cy.get('[data-cy=foo]').clear().type('bar123');
-    cy.get('[data-cy=create-client]').click();
+  it('Should migrate with setting scopes when constructing Auth0Client', function () {
+    cy.server();
 
+    cy.useV1({ clientScope: 'Scope1' });
+    cy.loginV1({ clientScope: 'Scope1' });
+
+    cy.useV2({ clientScope: 'Scope1' });
+
+    cy.get('[data-cy=profile]').should('exist');
     cy.get('[data-cy=profile]').should('not.contain', 'bar123');
 
-    cy.get('[data-cy=get-token-scope]').clear().type('Scope1 Scope2');
     cy.get('[data-cy=get-token]').click();
+    //cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
+
+  it('Should migrate with setting scopes when logging in', function () {
+    cy.server();
+
+    cy.useV1({});
+    cy.loginV1({ loginScope: 'Scope2' });
+
+    cy.useV2({});
+
+    cy.get('[data-cy=profile]').should('exist');
+    cy.get('[data-cy=profile]').should('not.contain', 'bar123');
+
+    cy.get('[data-cy=get-token]').click();
+    //cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
+
+  it('Should migrate when setting scopes when constructing Auth0Client and logging in', function () {
+    cy.server();
+
+    cy.useV1({ clientScope: 'Scope1' });
+    cy.loginV1({ clientScope: 'Scope1', loginScope: 'Scope2' });
+
+    cy.useV2({ clientScope: 'Scope1' });
+
+    cy.get('[data-cy=profile]').should('exist');
+    cy.get('[data-cy=profile]').should('not.contain', 'bar123');
+
+    cy.get('[data-cy=get-token]').click();
+    //cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
+
+  it('Should migrate without setting any scopes using custom claim', function () {
+    cy.server();
+
+    cy.useV1({ foo: 'bar123' });
+    cy.loginV1({});
+
+    cy.useV2({ foo: 'bar123' });
+
+    cy.get('[data-cy=profile]').should('exist');
     cy.get('[data-cy=profile]').should('contain', 'bar123');
   });
 
-  function runScenarioWithoutFoo({ clientScope, loginScope }) {
-    cy.visit('http://localhost:3000/v1.html');
+  it('Should migrate with setting scopes when constructing Auth0Client using custom claim', function () {
+    cy.server();
 
-    // create client
-    clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
-    cy.get('[data-cy=create-client]').click();
+    cy.useV1({ clientScope: 'Scope1', foo: 'bar123' });
+    cy.loginV1({ clientScope: 'Scope1' });
 
-    // logout
-    cy.get('[data-cy=logout]').click();
+    cy.useV2({ clientScope: 'Scope1' });
 
-    // login
-    // set login scope
-    loginScope && cy.get('[data-cy=login-scope]').clear().type(loginScope);
-    cy.get('[data-cy=login]').click();
-
-    // create client
-    clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
-    cy.get('[data-cy=create-client]').click();
-
-    // handle callback
-    cy.get('[data-cy=handle-redirect-callback]').click();
-
-    // expect user to show up
     cy.get('[data-cy=profile]').should('exist');
-
-    // go to v2
-    cy.visit('http://localhost:3000/v2.html');
-
-    // create client
-    clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
-    cy.get('[data-cy=foo]').clear().type('bar123');
-    cy.get('[data-cy=create-client]').click();
-
-    // expect user to show up
-    cy.get('[data-cy=profile]').should('exist');
-    cy.get('[data-cy=profile]').should('not.contain', 'bar123');
-
-    cy.get('[data-cy=get-token]').click();
     cy.get('[data-cy=profile]').should('contain', 'bar123');
-  }
+  });
 
-  function runScenarioWithFoo({ clientScope, loginScope, foo }) {
-    cy.visit('http://localhost:3000/v1.html');
+  it('Should migrate with setting scopes when logging in using custom claim', function () {
+    cy.server();
 
-    // create client
-    clientScope && cy.get('[data-cy=client-scope]').clear().type(clientScope);
-    foo && cy.get('[data-cy=foo]').clear().type(foo);
-    cy.get('[data-cy=create-client]').click();
+    cy.useV1({ foo: 'bar123' });
+    cy.loginV1({ loginScope: 'Scope2' });
 
-    // logout
-    cy.get('[data-cy=logout]').click();
+    cy.useV2({});
 
-    // login
-    // set login scope
-    loginScope && cy.get('[data-cy=login-scope]').clear().type(loginScope);
-    cy.get('[data-cy=login]').click();
-
-    // create client
-    cy.get('[data-cy=create-client]').click();
-
-    // handle callback
-    cy.get('[data-cy=handle-redirect-callback]').click();
-
-    // expect user to show up
     cy.get('[data-cy=profile]').should('exist');
+    cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
 
-    // go to v2
-    cy.visit('http://localhost:3000/v2.html');
+  it('Should migrate when setting scopes when constructing Auth0Client and logging in using custom claim', function () {
+    cy.server();
 
-    // create client
-    foo && cy.get('[data-cy=foo]').clear().type(foo);
-    cy.get('[data-cy=create-client]').click();
+    cy.useV1({ clientScope: 'Scope1', foo: 'bar123' });
+    cy.loginV1({ clientScope: 'Scope1', loginScope: 'Scope2' });
 
-    // expect user to show up
+    cy.useV2({ clientScope: 'Scope1' });
+
     cy.get('[data-cy=profile]').should('exist');
-    foo && cy.get('[data-cy=profile]').should('contain', foo);
-  }
+    cy.get('[data-cy=profile]').should('contain', 'bar123');
+  });
 });
