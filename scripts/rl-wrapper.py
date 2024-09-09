8a83609287c10686d2af3437b282dba3acadee97
Kunal Dawar <kunal.dawar@okta.com>
2024-09-09 15:27:35 +0530

    Updated python script

--- scripts/rl-wrapper.py
+++ scripts/rl-wrapper.py
@@ -86,13 +86,13 @@ def generate_report(rlsecure_path, workdir, targetdir, artifact_name, artifact_v
     except subprocess.CalledProcessError as e:
         sys.exit(f'[x] Failed to generate report: {e}')
 
-def detect_malware(report_file,workdir, artifact_name, artifact_version, repository, commit, build_env):
+def detect_malware(report_file, artifact_name, artifact_version, repository, commit, build_env):
     report_data = load_report(report_file)
     try:
         report_metadata = report_data['report']['metadata']
         malware_violation_rule_ids = MALWARE_VIOLATION_IDS
 
-        is_malware_detected = process_and_export_violations(report_metadata, workdir, malware_violation_rule_ids, artifact_name, artifact_version, repository, commit, build_env)
+        is_malware_detected = process_and_export_violations(report_metadata, malware_violation_rule_ids, artifact_name, artifact_version, repository, commit, build_env)
 
         if not is_malware_detected:
             print('[i] No Malware was detected.')
@@ -110,7 +110,7 @@ def load_report(report_file):
 
 import sys
 
-def process_and_export_violations(report_metadata, workdir, malware_violation_rule_ids, artifact_name, artifact_version, repository, commit, build_env):
+def process_and_export_violations(report_metadata, malware_violation_rule_ids, artifact_name, artifact_version, repository, commit, build_env):
     print('----------------- Detections -----------------', file=sys.stderr)
 
     is_malware_detected = False
@@ -127,10 +127,10 @@ def process_and_export_violations(report_metadata, workdir, malware_violation_ru
 
                 report_malware_detection(violation['rule_id'])
 
+
     file_name = 'violations.txt'
-    file_path = os.path.join(workdir, file_name)
     print('------------------RL Wrapper Scanner Save Violations------------------', file=sys.stderr)
-    with open(file_path, 'w') as file:
+    with open(file_name, 'w') as file:
         file.write('## 🚨 RL Wrapper Scanner Results: Malware Detected\n\n')
         file.write(f'**Artifact:** {artifact_name}\n')
         file.write(f'**Version:** {artifact_version}\n')
@@ -270,7 +270,7 @@ def main():
     scan_artifact(rlsecure_path, args.artifact, workdir, args.name, args.version)
     generate_report(rlsecure_path, workdir, targetdir, args.name, args.version)
 
-    is_non_compliant_violations = detect_malware(f'{workdir}/{targetdir}/report.rl.json', workdir, args.name, args.version, args.repository, args.commit, args.build_env)
+    is_non_compliant_violations = detect_malware(f'{workdir}/{targetdir}/report.rl.json', args.name, args.version, args.repository, args.commit, args.build_env)
 
     s3_results_path = submit_to_s3(workdir, targetdir, s3_bucket_name, tool_name, args.name, args.version, timestamp)
 
