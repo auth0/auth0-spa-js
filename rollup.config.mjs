0a3dc1f713fe7be8bffd15276c14e54fc154ab74
Yogesh Chaudhary <yogesh.chaudhary@okta.com>
2025-12-18 14:04:29 +0530

    Fix: Convert Rollup config to ESM for oidc-provider compatibility

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -9,9 +9,9 @@ import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 import replace from '@rollup/plugin-replace';
 import analyze from 'rollup-plugin-analyzer';
 import dev from 'rollup-plugin-dev';
-import { createApp } from './scripts/oidc-provider';
+import { createApp } from './scripts/oidc-provider.mjs';
 
-import pkg from './package.json';
+import pkg from './package.json' with { type: 'json' };
 
 const EXPORT_NAME = 'auth0';
 
