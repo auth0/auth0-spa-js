b861c5f111ffe67ffcf29b25e5013d1a3da1fd73
Stephen Phung <phungstephen@outlook.com>
2026-02-18 16:12:07 -0500

    Fix misspelling of `authenticate` in `createAuth0Client` docstring

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ export * from './global';
 /**
  * Asynchronously creates the Auth0Client instance and calls `checkSession`.
  *
- * **Note:** There are caveats to using this in a private browser tab, which may not silently authenticae
+ * **Note:** There are caveats to using this in a private browser tab, which may not silently authenticate
  * a user on page refresh. Please see [the checkSession docs](https://auth0.github.io/auth0-spa-js/classes/Auth0Client.html#checksession) for more info.
  *
  * @param options The client options
